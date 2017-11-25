27d14a0dfe3af1afdb9806e34b931d4ed6fd8aff
Kristupas <kristupas.vaitkus@ktu.edu>
2017-11-25 13:44:05 +0200

    Animal tables

---
+++ database/migrations/2017_11_25_114250_create_animal_types_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnimalTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('animal_types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('animal_types');
+    }
+}
